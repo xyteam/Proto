2c4b7834544f9d314289adae7aac14995a0caed6
xywang68 <xywang68@gmail.com>
2018-10-07 17:33:26 -0700

    more precise error detection for proxy errors

--- Examples/features/step_definitions/Then_I_should_see_the_title_of_#.js
+++ Examples/features/step_definitions/Then_I_should_see_the_title_of_#.js
@@ -3,5 +3,6 @@ module.exports = function() {
   this.Given(/^I should see the title contains the keyword of "([^"]*)"$/, {timeout: process.env.StepTimeoutInMS}, function (keyword) {
     // Write the automation code here
     expect(browser.getTitle().toLowerCase()).toContain(keyword);
+    expect(browser.getTitle().toLowerCase()).not.toEqual(keyword + '.com');
   });
 };
